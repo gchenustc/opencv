fc775bf2d71388637253bb0942347ac7a44a61d7
gchen <gchenn@mail.ustc.edu.cn>
2022-09-16 11:32:17 +0800

    update

--- 信用卡数字识别/0_argparse.py
+++ 信用卡数字识别/0_argparse.py
@@ -53,3 +53,46 @@ def demo5():
         print(f"{args.square}**2 = {args.square ** 2}")
     else:
         print(args.square ** 2)
+
+
+# default
+def demo6():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x", type=int, help="base")
+    parser.add_argument("y", type=int, help="exponent")
+    parser.add_argument("-v",'--verbose',action="count", default=0) # 不设定默认值，当不传入-v时，判断那里会出错
+    args = parser.parse_args()
+    answer = args.x ** args.y
+    
+    if args.verbose>=2: # -v/--verbose出现两次, 如果不出现 args.verbose 为None
+        print(f"{args.x} to the power {args.y} is {answer}")
+    elif args.verbose>=1:
+        print(f"{args.x}^{args.y}={answer}")
+    else:
+        print(answer)
+    # python .\0_argparse.py 2 3 -vv --> 2 to the power 3 is 8
+    # python .\0_argparse.py 2 3 --> 8
+
+# argparse.add_multually_exclusive_group() # 添加互斥的选项
+# argparse.ArgumentParser(description="") # description
+def demo7():
+    parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+    # ! -v 和 -q 只能出现其中一个
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v","--verbose",action="store_true",help="increase output verbosity")
+    group.add_argument("-q","--quiet",action="store_true",help="decrease output verbosity")
+
+    parser.add_argument("x", type=int, help="base")
+    parser.add_argument("y", type=int, help="exponent")
+    args = parser.parse_args()
+    answer = args.x ** args.y
+    
+    if args.quiet:
+        print(answer)
+    elif args.verbose:
+        print(f"{args.x} to the power {args.y} is {answer}")
+    else:
+        print(f"{args.x}^{args.y}={answer}")
+    
+demo7()
+    
