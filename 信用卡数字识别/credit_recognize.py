70a6b2ed3309b0d478a3e6b9c777d9135efe6d00
gchen <gchenn@mail.ustc.edu.cn>
2022-09-19 12:04:22 +0800

    update

--- 信用卡数字识别/credit_recognize.py
+++ 信用卡数字识别/credit_recognize.py
@@ -51,7 +51,7 @@ def cv_show(img):
 ap = argparse.ArgumentParser()
 ap.add_argument("-i","--image",required=True,help="path for input image")
 ap.add_argument("-t","--template",required=True,help="path for template image")
-args = vars(ap.parse_args("-i images/credit_card_01.png\
+args = vars(ap.parse_args("-i images/credit_card_03.png\
                           -t images/ocr_a_reference.png".split()))
 # 模板图像
 img_tem = cv2.imread(args["template"])
@@ -76,10 +76,62 @@ for index, (x,y,w,h) in enumerate(ref_rect):
     # cv_show(roi)
     digits[index] = roi
     
+# 卷积核
+kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (9,3))
+kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+
 # 读取输入图像，预处理
-img = cv2.imread("images/credit_card_01.png")
+img = cv2.imread(args["image"])
+# cv_show(img)
 img = resize(img, width=300)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv_show(img_gray)
 
+# 礼帽操作 - 原始-开运算（先腐蚀再膨胀）
+# 礼貌操作 - 突出明亮区域
+img_gray_tophat = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, kernel=kernel1)
+# cv_show(img_gray_tophat)
 
+# Sobel求梯度
+img_gradx = cv2.Sobel(img_gray_tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1) # ksize=-1 相当于 3*3
+img_gradx = np.absolute(img_gradx)
+img_grady = cv2.Sobel(img_gray_tophat, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1) # ksize=-1 相当于 3*3
+img_grady = np.absolute(img_grady)
+img_grad = cv2.addWeighted(img_gradx, 0.5, img_grady, 0.5, 0)
+# cv_show(img_grad)
 
+# 求完梯度后数值归一化
+minval, maxval = np.min(img_grad),np.max(img_grad)
+# print(minval, maxval)
+img_grad = 255* ((img_gradx-minval)/ (maxval-minval))
+img_grad = img_grad.astype("uint8") # 这一步必不可少
+# cv_show(img_grad)
+
+#通过闭操作（先膨胀，再腐蚀）将数字连在一起
+img_grad_close = cv2.morphologyEx(img_grad, cv2.MORPH_CLOSE, kernel1)
+# cv_show(img_grad_close)
+
+# thresh=0，自动划分阈值
+img_thresh = cv2.threshold(img_grad_close, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# cv_show(img_thresh)
+
+# 闭操作，先膨胀再腐蚀，使得相连的区域连接
+img_thresh = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, kernel2)
+# cv_show(img_thresh)
+
+# 计算轮廓
+contours = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+img_exhib = cv2.drawContours(img.copy(), contours, -1, (0,0,255), 1)
+# cv_show(img_exhib)
+
+locs_lst = []
+# 筛选需要的轮廓
+for index, cnt in enumerate(contours):
+    (x,y,w,h) = cv2.boundingRect(cnt)
+    ar = w / float(h)
+    if ar > 2.5 and ar < 4:
+        if (w>30 and w<55) and (h>10 and h<20):
+            locs_lst.append(cnt)
+
+# 对轮廓排序 - 按照x轴排序
+locs = sorted(locs_lst, key=lambda x:x[0])
